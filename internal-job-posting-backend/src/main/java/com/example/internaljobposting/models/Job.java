4269781c34e3dc5b246718e04488e2448d710432
Github_User <96969714+Saket109@users.noreply.github.com>
2025-02-16 22:56:45 +0530

    Update Job.java

--- internal-job-posting-backend/src/main/java/com/example/internaljobposting/models/Job.java
+++ internal-job-posting-backend/src/main/java/com/example/internaljobposting/models/Job.java
@@ -11,7 +11,7 @@ public class Job {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
+    private String title;
     private String description;
-    private String category;
+    private String department;
 }
