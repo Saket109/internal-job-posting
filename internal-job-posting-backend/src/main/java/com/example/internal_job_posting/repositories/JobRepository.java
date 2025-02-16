4922575127783624f641e2a8fbeb28ceb5e5c190
Github_User <96969714+Saket109@users.noreply.github.com>
2025-02-16 20:05:33 +0530

    Delete internal-job-posting-backend/src/main/java/com/example/internal_job_posting directory

--- internal-job-posting-backend/src/main/java/com/example/internal_job_posting/repositories/JobRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.internal_job_posting.repositories;
-
-import com.example.internal_job_posting.models.Job;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JobRepository extends JpaRepository<Job, Long> {
-}
