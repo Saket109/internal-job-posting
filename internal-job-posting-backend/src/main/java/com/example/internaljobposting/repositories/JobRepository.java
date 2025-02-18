7f48d709130e33b09101d89d14a8e71db4b704bc
Github_User <96969714+Saket109@users.noreply.github.com>
2025-02-18 11:42:10 +0530

    Update JobRepository.java

--- internal-job-posting-backend/src/main/java/com/example/internaljobposting/repositories/JobRepository.java
+++ internal-job-posting-backend/src/main/java/com/example/internaljobposting/repositories/JobRepository.java
@@ -1,6 +1,6 @@
-package com.example.internal_job_posting.repositories;
+package com.example.internaljobposting.repositories;
 
-import com.example.internal_job_posting.models.Job;
+import com.example.internaljobposting.models.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
